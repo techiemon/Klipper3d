d042adebe6cc5272d932a363d42fd7358b2a4591
John Hamson <jhamson@techiemon.com>
2025-08-12 16:54:46 -0500

    compiling issues

--- src/ssr_pwm.c
+++ src/ssr_pwm.c
@@ -5,25 +5,24 @@
 #include "sched.h"
 #include "board/gpio.h"
 
-static uint8_t ssr_pwm_pin;
+static struct gpio_pwm ssr_pwm;
 static uint32_t ssr_pwm_cycle_ticks = 0;
 static uint16_t ssr_pwm_value = 0;
 
 void
 ssr_pwm_init(uint8_t pin, uint32_t cycle_ticks)
 {
-    ssr_pwm_pin = pin;
     ssr_pwm_cycle_ticks = cycle_ticks;
     ssr_pwm_value = 0;
     // Configure the pin for PWM
-    gpio_pwm_setup(pin, cycle_ticks, 0);
+    ssr_pwm = gpio_pwm_setup(pin, cycle_ticks, 0);
 }
 
 void
 ssr_pwm_set(uint16_t value)
 {
     ssr_pwm_value = value;
-    gpio_pwm_write(ssr_pwm_pin, value);
+    gpio_pwm_write(ssr_pwm, value);
 }
 
 uint16_t
